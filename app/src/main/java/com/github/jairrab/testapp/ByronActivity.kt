d7bc93538dd1eb52072aeba9211f4f45a9a58f52
Jay Barria <jaybarria@gmail.com>
2020-10-26 11:48:23 +0800

    Updated app logic in ByronActivity.kt

--- app/src/main/java/com/github/jairrab/testapp/ByronActivity.kt
+++ app/src/main/java/com/github/jairrab/testapp/ByronActivity.kt
@@ -1,42 +1,85 @@
 package com.github.jairrab.testapp
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.github.jairrab.mathutils.isWholeNumber
+import com.github.jairrab.testapp.databinding.ActivityMainBinding
+import com.github.jairrab.testapp.petexercise.Pet
+import com.github.jairrab.testapp.petexercise.coleen.PetOfColeen
+import com.github.jairrab.testapp.petexercise.jay.PetOfJay
+import com.github.jairrab.testapp.petexercise.jemboy.PetofJemboy
+import com.github.jairrab.testapp.petexercise.rex.PetOfRex
+import com.github.jairrab.testapp.petexercise.ron.PetOfRon
+import java.util.*
 
 class ByronActivity : AppCompatActivity() {
-    //onCreate, onStart, onresume are lifecycle events
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         Log.v("lifecycle_event", "Event onCreate() called")
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        this.binding = binding
+        setContentView(binding.root)
+
+        binding.eating.setOnClickListener {
+            val currentPet = getCurrentPet()
+            currentPet.feed()
+            binding.petActivity.text = currentPet.getStatus()
+
+            it.hideKeyboard()
+        }
+
+        binding.playing.setOnClickListener {
+            val currentPet = getCurrentPet()
+            currentPet.play()
+            binding.petActivity.text = currentPet.getStatus()
+
+            it.hideKeyboard()
+        }
+
+        binding.sleeping.setOnClickListener {
+            val currentPet = getCurrentPet()
+            currentPet.sleep()
+            binding.petActivity.text = currentPet.getStatus()
+
+            it.hideKeyboard()
+        }
+
+        binding.clearActivity.setOnClickListener {
+            binding.petName.text = null
+            binding.petActivity.text = null
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.v("lifecycle_event", "Event onStart() called")
+    private fun getCurrentPet(): Pet {
+        val petName = binding?.petName?.text.toString()
+        val petNameLowerCase = petName.toLowerCase(Locale.getDefault())
+
+        if (petNameLowerCase == "byron") {
+            return PetOfJay()
+        } else if (petNameLowerCase == "coleen") {
+            return PetOfColeen()
+        } else if (petNameLowerCase == "jemboy") {
+            return PetofJemboy()
+        } else if (petNameLowerCase == "rex") {
+            return PetOfRex()
+        } else if (petNameLowerCase == "ron") {
+            return PetOfRon()
+        } else {
+            Toast.makeText(this, "Pet is not found, returning Jay's pet", Toast.LENGTH_SHORT).show()
+            return PetOfJay()
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.v("lifecycle_event", "Event onResume() called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.v("lifecycle_event", "Event onPause() called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.v("lifecycle_event", "Event onStop() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.v("lifecycle_event", "Event onDestroy() called")
+    private fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
     }
 }
