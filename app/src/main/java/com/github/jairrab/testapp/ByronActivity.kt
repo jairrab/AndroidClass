4b87ddc1c57ca5bd26278a96f91bc270cabf7d43
Jay Barria <jaybarria@gmail.com>
2020-10-27 21:43:46 +0800

    UI XML updates

--- app/src/main/java/com/github/jairrab/testapp/ByronActivity.kt
+++ app/src/main/java/com/github/jairrab/testapp/ByronActivity.kt
@@ -6,37 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.github.jairrab.mathutils.isWholeNumber
 
 class ByronActivity : AppCompatActivity() {
-    //onCreate, onStart, onresume are lifecycle events
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         Log.v("lifecycle_event", "Event onCreate() called")
-    }
 
-    override fun onStart() {
-        super.onStart()
-        Log.v("lifecycle_event", "Event onStart() called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.v("lifecycle_event", "Event onResume() called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.v("lifecycle_event", "Event onPause() called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.v("lifecycle_event", "Event onStop() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.v("lifecycle_event", "Event onDestroy() called")
+        setContentView(R.layout.activity_main)
     }
 }
